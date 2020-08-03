278fac66ec5e343c52f6683fc6ccc1e53fb5ff6f
Josh Koike <jk@tilting.me>
2020-08-02 17:04:52 -0700

    Improve hostside script, update python-usb-protocol remote

--- orangecrab_sketches/usb_hostside.py
+++ orangecrab_sketches/usb_hostside.py
@@ -1,6 +1,15 @@
-import evdev
-devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-orangecrab = [dev for dev in devices if "OrangeCrab" in dev.name][0]
-for event in orangecrab.read_loop():
-    if event.code:
-        print(event)
+import evdev, asyncio
+from collections import deque
+from collections import deque
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.ticker import FuncFormatter
+
+async def read_adc():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    orangecrab = [dev for dev in devices if "OrangeCrab" in dev.name][0]
+    async for event in orangecrab.async_read_loop():
+       if event.code:
+           val_pct = event.value / 255
+           print("#" * int(val_pct * 100))
+asyncio.run(read_adc())
